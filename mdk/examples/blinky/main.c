efd2537a455b64899d85b8d7a7ad7bb153f077b2
deLini1612 <ling.npl.1612@gmail.com>
2023-04-15 11:37:18 +0700

    Add

---
+++ mdk/examples/blinky/main.c
@@ -0,0 +1,19 @@
+#include <mdk.h>
+
+//static int led_pin = 3;
+static int led_pin = LED1;  // To override: make EXTRA_CFLAGS=-DLED1=5
+static int led_state = 0;
+
+int main(void) {
+  gpio_output(led_pin);
+  wdt_disable();
+
+  for (;;) {
+    printf("LED: %d\n", led_state);  // Print current state to console
+    gpio_write(led_pin, led_state);  // Blink an LED
+    led_state = !led_state;          // Toggle state
+    delay_ms(888);                   // Delay a bit
+  }
+
+  return 0;
+}
