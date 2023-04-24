d32e78f0b3b888b4f632dde5658e0028c2c531c2
deLini1612 <85008399+deLini1612@users.noreply.github.com>
2023-04-24 03:27:42 +0000

    Modify Lab2

--- Lab2/main.c
+++ Lab2/main.c
@@ -9,25 +9,25 @@ static int sw_pin = 1;
 int event()
 {
     sw_state = gpio_read(sw_pin);
-    printf("\n SWITCH: %s", (sw_state)?"OFF":"ON");
-    while (!sw_state)
-    {
-        delay_ms(10);
-        sw_state = gpio_read(sw_pin);
-    }
-    printf("\n SWITCH: %s", (sw_state)?"OFF":"ON");
+    printf("\n SWITCH: %s", (sw_state)?"ON":"OFF");
     while (sw_state)
     {
         delay_ms(10);
         sw_state = gpio_read(sw_pin);
     }
-    printf("\n SWITCH: %s", (sw_state)?"OFF":"ON");
+    printf("\n SWITCH: %s", (sw_state)?"ON":"OFF");
     while (!sw_state)
     {
         delay_ms(10);
         sw_state = gpio_read(sw_pin);
     }
-    printf("\n SWITCH: %s", (sw_state)?"OFF":"ON");
+    printf("\n SWITCH: %s", (sw_state)?"ON":"OFF");
+    while (sw_state)
+    {
+        delay_ms(10);
+        sw_state = gpio_read(sw_pin);
+    }
+    printf("\n SWITCH: %s", (sw_state)?"ON":"OFF");
     return 1;
 }
 
@@ -45,7 +45,7 @@ int main(void)
         gpio_write(led_pin, led_state);
         printf("\n Blink LED");
         led_state = !led_state;
-        delay_ms(500);
+        delay_ms(300);
     }
 
     return 0;
